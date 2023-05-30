4bab11254790ad7532fe53a873ed0ce582d4dab5
abhishekchauhan98 <abhishek.chauhan1@tothenew.com>
2023-05-30 17:47:30 +0530

    updated user data path

--- main.tf
+++ main.tf
@@ -99,7 +99,7 @@ data "aws_ami" "amazon-linux-2" {
 }
 
 data "template_file" "user_data" {
-  template = file(var.master_user_data_path? var.master_user_data_path : "${path.module}/user_data.sh")
+  template = file(var.master_user_data_path == "" ? "${path.module}/user_data.sh" : var.master_user_data_path)
   vars = {
     environment_name = var.environment_name
     region           = var.region
@@ -138,7 +138,7 @@ resource "aws_instance" "ec2_rabbitmq_master" {
 }
 
 data "template_file" "user_data_worker" {
-  template = file(var.worker_user_data_path ? var.worker_user_data_path : "${path.module}/worker.sh")
+  template = file(var.worker_user_data_path == "" ? "${path.module}/worker.sh" : var.worker_user_data_path)
   vars = {
     environment_name = var.environment_name
     region           = var.region
